39722d140c3a06be8b5211691e5bb597051007ab
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-09-24 14:49:41 +0200

    Second commmit, I create two function with one listener for each input

--- main.js
+++ main.js
@@ -1,63 +1,74 @@
-// let checkboxSupport = document.querySelector(".checkboxSupport");
+let checkSupport = () => {
+  let checkboxSupport = document.querySelector(".checkboxSupport");
+  let support = document.querySelector(".support");
 
-// let enquiry = document.querySelector(".enquiry");
-// let support = document.querySelector(".support");
+  checkboxSupport.addEventListener("click", () => {
+    if (checkboxSupport.checked) {
+      support.style.setProperty("--background-support", "#f66f");
+    } else {
+      support.style.setProperty("--background-support", "#252525");
+    }
+  });
+};
 
-// checkboxSupport.addEventListener("click", () => {
-//   if (checkboxSupport.checked) {
-//     support.style.setProperty("--background-support", "#f66f");
-//     console.log("check");
-//   } else {
-//     support.style.setProperty("--background-support", "#252525");
-//     console.log("nocheck");
-//   }
-// });
+/////
 
-// /////
+let checkEnquiry = () => {
+  let checkboxEnquiry = document.querySelector(".checkboxEnquiry");
+  let enquiry = document.querySelector(".enquiry");
 
-// let checkboxEnquiry = document.querySelector(".checkboxEnquiry");
+  checkboxEnquiry.addEventListener("click", () => {
+    if (checkboxEnquiry.checked) {
+      enquiry.style.setProperty("--background-enquiry", "#f66f");
+    } else {
+      enquiry.style.setProperty("--background-enquiry", "#252525");
+    }
+  });
+};
 
-// checkboxEnquiry.addEventListener("click", () => {
-//   if (checkboxEnquiry.checked) {
-//     enquiry.style.setProperty("--background-enquiry", "#f66f");
-//     console.log("esta checkeado");
-//   } else {
-//     enquiry.style.setProperty("--background-enquiry", "#252525");
-//     console.log("no esta checkeado");
-//   }
-// });
+checkSupport();
+checkEnquiry();
 
 /*  YA PUDIMOS CHECKEAR SI EL INPUT ESTA CHECKEADO O NO, AHORA TENEMOS QUE CAMBIAR LA PROPIEDAD --BACKGROUND CUANDO EL ELEMENTO ESTA CHECKED */
 
-class Checker {
-  contructor(checkboxSupport, support, checkboxEnquiry, enquiry) {
-    this.checkboxSupport = checkboxSupport;
-    this.support = support;
-    this.checkboxEnquiry = checkboxEnquiry;
-    this.enquiry = enquiry;
+// class Checker {
+//   contructor(checkboxSupport, support, checkboxEnquiry, enquiry) {
+//     this.checkboxSupport = document.querySelector(checkboxSupport);
+//     this.support = document.querySelector(support);
+//     this.checkboxEnquiry = document.querySelector(checkboxEnquiry);
+//     this.enquiry = document.querySelector(enquiry);
+//     this.checkSupport();
+//     this.checkEnquiry();
+//   }
+//   // Listener for checkbox Support
 
-    // Listener for checkbox Support
-    checkboxSupport.addEventListener("click", () => {
-      if (checkboxSupport.checked) {
-        support.style.setProperty("--background-support", "#f66f");
-        console.log("check");
-      } else {
-        support.style.setProperty("--background-support", "#252525");
-        console.log("nocheck");
-      }
-    });
+//   checkSupport() {
+//     checkboxSupport.addEventListener("click", () => {
+//       if (checkboxSupport.checked) {
+//         support.style.setProperty("--background-support", "#f66f");
+//         console.log("esta checkeado");
+//       } else {
+//         support.style.setProperty("--background-support", "#252525");
+//         console.log("no esta checkeado");
+//       }
+//     });
+//   }
 
-    // Listener for checkbox Enquiry
-    checkboxEnquiry.addEventListener("click", () => {
-      if (checkboxEnquiry.checked) {
-        enquiry.style.setProperty("--background-enquiry", "#f66f");
-        console.log("esta checkeado");
-      } else {
-        enquiry.style.setProperty("--background-enquiry", "#252525");
-        console.log("no esta checkeado");
-      }
-    });
-  }
-}
+//   // Listener for checkbox Enquiry
+//   checkEnquiry() {
+//     checkboxEnquiry.addEventListener("click", () => {
+//       if (checkboxEnquiry.checked) {
+//         enquiry.style.setProperty("--background-enquiry", "#f66f");
+//         console.log("esta checkeado");
+//       } else {
+//         enquiry.style.setProperty("--background-enquiry", "#252525");
+//         console.log("no esta checkeado");
+//       }
+//     });
+//   }
+// }
 
-new Checker(".checkboxSupport", ".support", ".checkboxEnquiry", ".enquiry");
+// (function () {
+//   new Checker(".checkboxSupport", ".support", ".checkboxEnquiry", ".enquiry");
+//   console.log("ejecutando");
+// })();
