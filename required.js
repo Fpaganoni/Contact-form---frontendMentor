f18b2c0ee583e06c0c448fe50dccc268ab63d17a
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-09-28 11:37:59 +0200

    I start to develop the required.js

--- required.js
+++ required.js
@@ -7,10 +7,25 @@ let consentRequired = document.querySelector("#consentRequired");
 let buttonSubmit = document.querySelector("#submit-button");
 
 // class error
+let formInput = document.querySelectorAll(".form-input");
+let emptyInputs = () => {
+  buttonSubmit.addEventListener("click", () => {
+    let showErrors = document.querySelectorAll(".error-required");
+    showErrors.forEach((e) => {
+      e.classList.toggle("error-required-show");
+    });
+  });
+};
 
 buttonSubmit.addEventListener("click", () => {
-  let showErrors = document.querySelectorAll(".error-required");
-  showErrors.classlist.add(".error-required-show");
+  formInput.forEach((e) => {
+    if (e.value.length == 0) {
+      emptyInputs();
+      e.style.border = "1.2px solid #d73c3c";
+    } else {
+      console.error("No esta funcionando");
+    }
+  });
 });
 
-/*  Terminado el modelado en modo celular y pc, con js tengo que mostrar los ERRORS, pintar de color rojo los inputs vacios, y validar datos */
+/*  Avance en el required.js ya modificamos unas cosas, tenemos dos funciones que cambian lor bordes de los inputs si estan vacios, y un msj required que aparece en pantalla cuando hacemos click en el submit ... Ahora tenemos que unirlo en una unica funcion, y que funcione con cada input en particular. */
