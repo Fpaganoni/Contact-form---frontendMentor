6e2f43c7e0cc0bc3c9bedc4ac93a03b4c023feef
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-09-30 19:26:28 +0200

    Already did the validation for de inputs type check

--- required.js
+++ required.js
@@ -1,13 +1,7 @@
 /* Paragraph errors */
 const firstNameRequired = document.querySelector("#firstNameRequired"),
   lasNameRequired = document.querySelector("#lastNameRequired"),
-  emailRequired = document.querySelector("#emailRequired"),
-  queryRequired = document.querySelector("#queryRequired"),
-  messageRequired = document.querySelector("#messageRequired"),
-  consentRequired = document.querySelector("#consentRequired");
-
-/* Inputs */
-let inputs = document.querySelectorAll(".form-input");
+  emailRequired = document.querySelector("#emailRequired");
 
 let inputName = document.querySelector("#first-name"),
   inputLastName = document.querySelector("#last-name"),
@@ -17,17 +11,14 @@ let validation = () => {
   // const nameRegex = /^[A-Za-záéíóúüñÑ\s]+$/; // Expresión regular para letras y espacios
   if (inputName.value === "") {
     inputName.style.border = "1px solid #d73c3c";
-    inputName.style.boxShadow = "3px 3px 10px #252525";
     firstNameRequired.style.display = "block";
   } else {
     inputName.style.border = "1px solid #87a3a6";
-    inputName.style.boxShadow = "none";
     firstNameRequired.style.display = "none";
   }
 
   if (inputLastName.value === "") {
     inputLastName.style.border = "1px solid #d73c3c";
-    inputLastName.style.boxShadow = "3px 3px 10px #252525";
     lasNameRequired.style.display = "block";
   } else {
     inputLastName.style.border = "1px solid #87a3a6";
@@ -36,20 +27,33 @@ let validation = () => {
 
   if (inputEmail.value === "") {
     inputEmail.style.border = "1px solid #d73c3c";
-    inputEmail.style.boxShadow = "3px 3px 10px #252525";
     emailRequired.style.display = "block";
   } else {
     inputEmail.style.border = "1px solid #87a3a6";
     emailRequired.style.display = "none";
-    inputEmail.style.boxShadow = "none";
   }
 };
 
 let buttonSubmit = document.querySelector("#submit-button");
 buttonSubmit.addEventListener("click", (e) => {
   e.preventDefault();
-
   validation();
+  queryValidation();
 });
 
-/*  Avance en el required.js ya modificamos unas cosas, tenemos dos funciones que cambian los bordes de los inputs si estan vacios, y un msj required que aparece en pantalla cuando hacemos click en el submit ... Ahora tenemos que unirlo en una unica funcion, y que funcione con cada input en particular. */
+/*  Validation of query inputs */
+
+const queryRequired = document.querySelector("#queryRequired"),
+  enquiryInput = document.querySelector("#generalEnquiry"),
+  requestInput = document.querySelector("#supportRequest");
+
+let queryValidation = () => {
+  if (!enquiryInput.checked && !requestInput.checked) {
+    queryRequired.style.display = "block";
+  } else {
+    queryRequired.style.display = "none";
+  }
+};
+
+const messageRequired = document.querySelector("#messageRequired"),
+  consentRequired = document.querySelector("#consentRequired");
