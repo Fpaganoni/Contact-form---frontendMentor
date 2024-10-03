5ee8a3d8c54e8c58a29677c723cc82e51ac8a279
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-10-03 15:38:21 +0200

    All inputs validations

--- required.js
+++ required.js
@@ -1,14 +1,14 @@
-/* Paragraph errors */
-const firstNameRequired = document.querySelector("#firstNameRequired"),
-  lasNameRequired = document.querySelector("#lastNameRequired"),
-  emailRequired = document.querySelector("#emailRequired");
+/* Validation of name, lastname and email inputs  */
 
-let inputName = document.querySelector("#first-name"),
-  inputLastName = document.querySelector("#last-name"),
-  inputEmail = document.querySelector("#email-address");
+const inputsValidation = () => {
+  const firstNameRequired = document.querySelector("#firstNameRequired"),
+    lasNameRequired = document.querySelector("#lastNameRequired"),
+    emailRequired = document.querySelector("#emailRequired");
 
-let validation = () => {
-  // const nameRegex = /^[A-Za-záéíóúüñÑ\s]+$/; // Expresión regular para letras y espacios
+  const inputName = document.querySelector("#first-name"),
+    inputLastName = document.querySelector("#last-name"),
+    inputEmail = document.querySelector("#email-address");
+
   if (inputName.value === "") {
     inputName.style.border = "1px solid #d73c3c";
     firstNameRequired.style.display = "block";
@@ -34,20 +34,12 @@ let validation = () => {
   }
 };
 
-let buttonSubmit = document.querySelector("#submit-button");
-buttonSubmit.addEventListener("click", (e) => {
-  e.preventDefault();
-  validation();
-  queryValidation();
-});
+/*  Validation of query type inputs */
 
-/*  Validation of query inputs */
-
-const queryRequired = document.querySelector("#queryRequired"),
-  enquiryInput = document.querySelector("#generalEnquiry"),
-  requestInput = document.querySelector("#supportRequest");
-
-let queryValidation = () => {
+const queryValidation = () => {
+  const queryRequired = document.querySelector("#queryRequired"),
+    enquiryInput = document.querySelector("#generalEnquiry"),
+    requestInput = document.querySelector("#supportRequest");
   if (!enquiryInput.checked && !requestInput.checked) {
     queryRequired.style.display = "block";
   } else {
@@ -55,5 +47,37 @@ let queryValidation = () => {
   }
 };
 
-const messageRequired = document.querySelector("#messageRequired"),
-  consentRequired = document.querySelector("#consentRequired");
+/* Validation of message input */
+
+const messageValidation = () => {
+  const messageRequired = document.querySelector("#messageRequired"),
+    messageInput = document.querySelector("#message-input");
+  if (messageInput.value.length === 0) {
+    messageInput.style.border = "1px solid #d73c3c";
+    messageRequired.style.display = "block";
+  } else {
+    messageInput.style.border = "1px solid gray";
+    messageRequired.style.display = "none";
+  }
+};
+
+/* Validation of content input */
+
+let consentValidation = () => {
+  const consentRequired = document.querySelector("#consentRequired"),
+    consentInput = document.querySelector("#consent-input");
+  if (!consentInput.checked) {
+    consentRequired.style.display = "block";
+  } else {
+    consentRequired.style.display = "none";
+  }
+};
+
+const buttonSubmit = document.querySelector("#submit-button");
+buttonSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputsValidation();
+  queryValidation();
+  messageValidation();
+  consentValidation();
+});
